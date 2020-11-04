4d0d4bdd96b2c829977098e503d435ca561933d1
hrishi036 <hrishikesh036@gmail.com>
2020-11-04 15:59:13 +0530

    completed central frequency config population

--- AnalysisPackages/dispersiondelay/pulsar_information_utility.py
+++ AnalysisPackages/dispersiondelay/pulsar_information_utility.py
@@ -8,6 +8,9 @@ Also holds the information in resources to be used in actual code
 import os
 from pathlib import Path
 import numpy as np
+import configparser
+
+config = configparser.ConfigParser()
 
 
 def get_intermediate_frequency(channel_number):
@@ -16,25 +19,48 @@ def get_intermediate_frequency(channel_number):
     return 140
 
 
+def config_write(filename):
+    with open(filename, 'w') as configfile:
+        config.write(configfile)
+
+
+def config_set_central_frequency(channel_number, central_freq):
+    try:
+        config.set('channel-' + str(channel_number) + '-specific', 'central_frequency', str(central_freq))
+    except configparser.NoSectionError:
+        print('[channel-' + str(channel_number) + '-specific] section not found. Creating new section')
+        config['channel-' + str(channel_number) + '-specific'] = {'central_frequency': str(central_freq)}
+
+
 class PulsarInformationUtility:
     def __init__(self, mbr_pulsar_name_date_time):
         self.psr_name = mbr_pulsar_name_date_time
 
-    def all_channels_central_frequency(self, populate_resources=False):
+    def all_channels_central_frequency(self, populate_config=True, populate_resources_file=False):
         root_dirname = str(Path(__file__).parent.parent.parent.absolute())
-        if populate_resources:
+        config_filename = root_dirname + '/AnalysisPackages/resources/config.txt'
+        output_filename = (root_dirname + "/AnalysisPackages/resources/ChannelVsDispersionPacketDelay_"
+                           + self.psr_name + ".txt")
+
+        if populate_config:
+            config.read(config_filename)
+            print("populate_config set to True. " + config_filename + " will be updated")
+        else:
+            print("populate_config set to False. " + config_filename + " will not be updated")
+
+        if populate_resources_file:
             output_filename = str(root_dirname +
                                   "/AnalysisPackages/resources/ChannelVsDispersionPacketDelay_"
                                   + self.psr_name + ".txt")
-            print("output file name     : ", output_filename)
-            output_file = open(output_filename, "w")
-            output_file.truncate(0)
-            output_file.close()
+            with open(output_filename, "w") as output_file:
+                output_file.truncate(0)
+            print("populate_resources_file set to True. " + output_filename + " will be updated\n\n")
+        else:
+            print("populate_resources_file set to False. " + output_filename + " will not be updated\n\n")
 
         output_array = []
         input_dirname = str(root_dirname + "/MBRData")
         print("input directory name : ", input_dirname)
-
 
         for channel_number in range(1, 10):
             if not os.path.exists(str(input_dirname) + "/ch0" + str(channel_number)):
@@ -51,14 +77,15 @@ class PulsarInformationUtility:
             central_freq = lo_freq - get_intermediate_frequency(channel_number)
             print("ch:0" + str(channel_number) + "	LO: " + str(lo_freq) + "	CF: "
                   + str(central_freq))
-            if populate_resources:
-                output_file = open(output_filename, "a")
-                output_file.write(str(channel_number) + "\t" + str(central_freq)+"\n")
-                output_file.close()
+            if populate_resources_file:
+                with open(output_filename, "a") as output_file:
+                    output_file.write(str(channel_number) + "\t" + str(central_freq) + "\n")
+            if populate_config:
+                config_set_central_frequency(channel_number, central_freq)
             output_array.append([channel_number, central_freq])
-
+        config_write(config_filename)
         return output_array
 
 
 psr = PulsarInformationUtility("B0834+06_20090725_114903")
-print(np.array(psr.all_channels_central_frequency()))
+print(np.array(psr.all_channels_central_frequency(False, False)))
--- AnalysisPackages/resources/config_sample.py
+++ AnalysisPackages/resources/config_sample.py
@@ -1,2 +1,11 @@
 import configparser
 
+config = configparser.ConfigParser()
+
+config.read('config.txt')
+#config.get('pulsar-config')
+config.set('pulsar-config', 'new', '3')
+config.set('pulsar-config', 'test', '5')
+
+with open('config.txt', 'w') as configfile:
+    config.write(configfile)
