699b39ec32d643768fdac992ce3fde0d3637ecbe
hrishi036 <hrishikesh036@gmail.com>
2021-07-12 19:36:48 +0530

    setup output dirs

--- AnalysisPackages/utilities/setup_dirs.py
+++ AnalysisPackages/utilities/setup_dirs.py
@@ -7,13 +7,14 @@ from AnalysisPackages.utilities.pulsar_information_utility import PulsarInformat
 def main(file_name):
     psr = PulsarInformationUtility(file_name)
     bands = int(psr.n_bands)
-
+    current_dir = str(Path.cwd())
     for band in range(bands):
-        Path(str(Path.cwd().parent.parent) + f"/OutputData/{file_name}/DynamicSpectrum/ch0{str(band)}/") \
+        print("saving : "+ current_dir + f"/OutputData/{file_name}/DynamicSpectrum/ch0{str(band)}/")
+        Path(current_dir + f"/OutputData/{file_name}/DynamicSpectrum/ch0{str(band)}/") \
             .mkdir(parents=True, exist_ok=True)
-        Path(str(Path.cwd().parent.parent) + f"/OutputData/{file_name}/AveragePulseProfile/ch0{str(band)}/") \
+        Path(current_dir + f"/OutputData/{file_name}/AveragePulseProfile/ch0{str(band)}/") \
             .mkdir(parents=True, exist_ok=True)
-        Path(str(Path.cwd().parent.parent) + f"/OutputData/{file_name}/TimeSeries/ch0{str(band)}/") \
+        Path(current_dir + f"/OutputData/{file_name}/TimeSeries/ch0{str(band)}/") \
             .mkdir(parents=True, exist_ok=True)
         # /Users/hrishikesh.s/RRIProject/pulsar-analysis-package/OutputData/B0834+06_20090725_114903/DynamicSpectrum/ch02/ch02_B0834+06_20090725_114903_imagXY.spec
 
