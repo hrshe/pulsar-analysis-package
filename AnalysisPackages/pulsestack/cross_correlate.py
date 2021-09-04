a9989c9d6a90bff62e0ae8e0a8b266e9aa781eae
hrishi036 <hrishikesh036@gmail.com>
2021-09-04 06:05:17 +0530

    generalize cross-corr

--- AnalysisPackages/pulsestack/cross_correlate.py
+++ AnalysisPackages/pulsestack/cross_correlate.py
@@ -12,59 +12,75 @@ from AnalysisPackages.utilities.pulsar_information_utility import PulsarInformat
 def main(pulsar_name, ref_ch_number, second_ch_number):
     ref_channel_number = int(ref_ch_number[2:4])
     second_channel_number = int(second_ch_number[2:4])
-    root_dirname = str(Path(__file__).parent.parent.parent.absolute()) + '/'
+    root_dirname =  str(Path(__file__).parent.parent.parent.absolute()) + '/'
     psr = PulsarInformationUtility(pulsar_name)
 
-    ref_pulse_stack_filename = utils.get_folded_pulse_stack_filename(ref_channel_number, root_dirname, psr)
+    ref_pulse_stack_filename = utils.get_smoothened_folded_pulse_stack_filename(ref_channel_number, root_dirname, psr)
     with open(ref_pulse_stack_filename) as ref_pulse_stack_file:
         ref_pulse_stack = np.loadtxt(ref_pulse_stack_file)
     # plt.imshow(ref_pulse_stack, origin="lower")
     # plt.show()
 
-    second_pulse_stack_filename = utils.get_folded_pulse_stack_filename(second_channel_number, root_dirname, psr)
+    second_pulse_stack_filename = utils.get_smoothened_folded_pulse_stack_filename(second_channel_number, root_dirname, psr)
     with open(second_pulse_stack_filename) as second_pulse_stack_file:
         second_pulse_stack = np.loadtxt(second_pulse_stack_file)
     # plt.imshow(second_pulse_stack, origin="lower")
     # plt.show()
+    print(second_pulse_stack.shape)
 
+    rows, cols = second_pulse_stack.shape
     correlation_matrix = np.zeros(ref_pulse_stack.shape)
-    for row_shift in range(-99, 101):  # range(-49, 51)
+    for row_shift in range(-int(rows/2 - 1), int(rows/2 + 1)):  # range(-99, 101)
         row_shifted = np.roll(second_pulse_stack, row_shift, axis=0)
-        for col_shift in range(-100, 99):  # range(-100, 99)
-            correlation_matrix[row_shift + 99, col_shift + 100] = np.sum(
+        for col_shift in range(-int((cols+1)/2), int(cols/2)):  # range(-100, 99)
+            correlation_matrix[row_shift + int(rows/2 - 1), col_shift + int((cols+1)/2)] = np.sum(
                 ref_pulse_stack * np.roll(row_shifted, col_shift, axis=1))
 
     # correlation_matrix = signal.correlate2d(ref_pulse_stack, second_pulse_stack)
 
-    plt.imshow(correlation_matrix, origin="lower", extent=[-100 / 1000, 99 / 1000, -99 / 100, 100 / 100],
+    plt.tight_layout()
+    plt.subplot(1,2,2)
+    plt.imshow(correlation_matrix, origin="lower", extent=[-100 / 1000, 99 / 1000, -49 / 100, 50 / 100],
                cmap="coolwarm", aspect="auto")
     plt.colorbar()
     plt.xlabel("Shift in pulse phase")
-    plt.ylabel("Shift in modulation phase")
-    plt.title(f"Cross correlation between band {ref_ch_number} and {second_ch_number}")
+    #plt.ylabel("Shift in modulation phase")
+    plt.title(f"Cross correlation between\nband {ref_ch_number} and {second_ch_number}")
     # plt.text(0.5, 0.5, "sample", col)
 
     position_max = np.unravel_index(correlation_matrix.argmax(), correlation_matrix.shape)
     print(position_max)
-    shift_pulse_phase = (position_max[1] - 100) / 1000
-    shift_modulation_phase = (position_max[0] - 99) / 200
+    shift_pulse_phase = (position_max[1] - int((cols+1)/2)) / 1000
+    shift_modulation_phase = (position_max[0] - int(rows/2 - 1)) / rows
+    plt.scatter(shift_pulse_phase, shift_modulation_phase, color="y", marker="x")
+    plt.annotate(f"Max: {round(correlation_matrix[position_max[0], position_max[1]], 2)} \n"
+                 f"({shift_pulse_phase},{shift_modulation_phase})", (shift_pulse_phase, shift_modulation_phase),
+                 xytext=(shift_pulse_phase + 0.002, shift_modulation_phase - 0.03))
+    print(f"shift_pulse_phase: {shift_pulse_phase}... shift_modulation_phase: {shift_modulation_phase}")
+    plt.subplot(1, 2, 1)
+    plt.ylim(-0.49, 0.5)
+    plt.ylabel("Shift in modulation phase")
+    plt.title(f"Max at each modulation phase\nband {ref_ch_number} and {second_ch_number}")
+    plt.plot(np.amax(correlation_matrix, axis=1), np.linspace(-0.49, 0.50, rows))
+    plt.show()
+
+    plt.imshow(correlation_matrix, origin="lower", extent=[-100 / 1000, 99 / 1000, -49 / 100, 50 / 100],
+               cmap="coolwarm", aspect="auto")
+    plt.colorbar()
+    plt.xlabel("Shift in pulse phase")
+    plt.ylabel("Shift in modulation phase")
+    plt.title(f"Cross correlation between\nband {ref_ch_number} and {second_ch_number}")
     plt.scatter(shift_pulse_phase, shift_modulation_phase, color="y", marker="x")
     plt.annotate(f"Max: {round(correlation_matrix[position_max[0], position_max[1]], 2)} \n"
                  f"({shift_pulse_phase},{shift_modulation_phase})", (shift_pulse_phase, shift_modulation_phase),
                  xytext=(shift_pulse_phase + 0.002, shift_modulation_phase - 0.03))
     plt.show()
-    print(f"shift_pulse_phase: {shift_pulse_phase}... shift_modulation_phase: {shift_modulation_phase}")
 
     plt.xlabel("Shift in modulation phase")
     plt.title(f"Cross correlation at max allignment in pulse phase\n"
               f"between bands {ref_ch_number} and {second_ch_number}")
-    plt.plot(np.linspace(-0.5, 0.495, 200), correlation_matrix[:, position_max[1]])
+    plt.plot(np.amax(correlation_matrix, axis=1), np.linspace(-0.5, 0.495, rows))
     plt.show()
-    # rows_grid, cols_grid = np.meshgrid(np.arange(0,199), np.arange(0,100))
-    # fig = plt.figure(figsize=[12,12])
-    # ax = fig.gca(projection="3d")
-    # ax.plot_surface(rows_grid, cols_grid, correlation_matrix, cmap=cm.coolwarm)
-    # plt.show()
 
 
 if __name__ == '__main__':
